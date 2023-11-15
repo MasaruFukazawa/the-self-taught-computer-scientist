c196663557864d9bd5f7075dd52339459b131dbb
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-11-16 01:39:49 +0900

    アナグラム検出

---
+++ tests/the_self_taught_computer_scientist/test_anagram.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+from the_self_taught_computer_scientist.anagram import is_anagram
+
+
+def test_is_anagram():
+    assert is_anagram("Emperor Octavian", "Captain over Rome")
