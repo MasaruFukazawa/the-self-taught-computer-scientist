093e5e59f770657c0f117ecbacef53b64dd332cd
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-12-26 22:55:20 +0900

    Listを使ったStackと最小値を保持するStackの実装

--- tests/the_self_taught_computer_scientist/test_stack.py
+++ tests/the_self_taught_computer_scientist/test_stack.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from the_self_taught_computer_scientist.stack import Stack
+from the_self_taught_computer_scientist.stack import ListStack, Stack
 
 
 def test_push_in_stack():
@@ -40,6 +40,52 @@ def test_is_empty():
 
 
 def test_peek_in_stack():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    assert stack.peek() == 5
+
+
+def test_push_in_liststack():
+    stack = ListStack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+    assert stack.size() == 5
+
+
+def test_pop_from_liststack():
+    stack = ListStack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    assert stack.pop() == 5
+    assert stack.pop() == 4
+
+
+def test_is_empty_liststack():
+    stack = ListStack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    stack.pop()
+    stack.pop()
+    stack.pop()
+
+    assert stack.is_empty()
+
+
+def test_peek_in_liststack():
     stack = Stack()
     stack.push(1)
     stack.push(2)
--- the_self_taught_computer_scientist/stack.py
+++ the_self_taught_computer_scientist/stack.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from typing import Optional
+
 
 class Stack:
     def __init__(self) -> None:
@@ -21,3 +23,91 @@ class Stack:
 
     def peek(self) -> int:
         return self.items[-1]
+
+
+class Node:
+    def __init__(self, data: int) -> None:
+        self.__data: int = data
+        self.__next: Optional[Node] = None
+
+    @property
+    def data(self) -> int:
+        return self.__data
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, node: Node):
+        self.__next = node
+
+
+class ListStack:
+    def __init__(self) -> None:
+        self.__head: Optional[Node] = None
+
+    def push(self, data: int) -> None:
+        node: Node = Node(data)
+
+        if self.__head is None:
+            self.__head = node
+        else:
+            node.next = self.__head
+            self.__head = node
+
+    def pop(self) -> int:
+        if self.__head is None:
+            raise IndexError("pop from empty stack")
+
+        popped_node: Node = self.__head
+
+        self.__head = self.__head.next
+
+        return popped_node.data
+
+    def size(self) -> int:
+        if self.is_empty():
+            return 0
+
+        node: Optional[Node] = self.__head
+        count: int = 0
+
+        while node:
+            count += 1
+            node = node.next
+
+        return count
+
+    def is_empty(self) -> bool:
+        if self.__head is None:
+            return True
+
+        return False
+
+    def peek(self) -> int:
+        if self.__head is None:
+            raise IndexError("pop from empty stack")
+
+        return self.__head.data
+
+
+class MinStack:
+    def __init__(self) -> None:
+        self.__main: list[int] = []
+        self.__min: list[int] = []
+
+    def push(self, data: int) -> None:
+
+        if len(self.__main) == 0:
+            self.__min.append(data)
+        elif data <= self.__min[-1]:
+            self.__min.append(data)
+        else:
+            self.__min.append(self.__min[-1])
+
+        self.__main.append(data)
+
+    def pop(self) -> int:
+        self.__min.pop()
+        return self.__main.pop()
